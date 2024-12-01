7dcec66993ca6ddf9cdb9a60d1b2c94bc3a4a6ed
Je-Mé Kruger-Baartjes <jmkb1502@gmail.com>
2024-12-01 19:08:33 +0200

    Adding Dark mode

---
+++ src/app/theme-toggle/theme-toggle.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ThemeService } from '../theme.service';
+import { MatIconModule } from '@angular/material/icon'; // Import MatIconModule
+
+@Component({
+  selector: 'app-theme-toggle',
+  templateUrl: './theme-toggle.component.html',
+  standalone: true, // Ensure it's standalone
+  imports: [MatIconModule], // Add MatIconModule here
+  styleUrls: ['./theme-toggle.component.css'],
+})
+export class ThemeToggleComponent {
+  isDarkMode: boolean;
+
+  constructor(private themeService: ThemeService) {
+    this.isDarkMode = this.themeService.isDarkMode();
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    this.themeService.setDarkMode(this.isDarkMode);
+  }
+}
