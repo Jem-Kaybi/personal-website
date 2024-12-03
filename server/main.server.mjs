0a97f1ae40240bac94c707c6c7b33b9c15cdc797
Jem-Kaybi <Jem-Kaybi@users.noreply.github.com>
2024-12-03 10:48:40 +0000

    deploy: 347b999d4932622ae8c88a2be4d0776e192b64b4

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7XMD5527.mjs";import"./chunk-FEPJREAB.mjs";import"./chunk-3FWSP2DM.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-WL5SKKQ7.mjs";import"./chunk-FEPJREAB.mjs";import"./chunk-3FWSP2DM.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
