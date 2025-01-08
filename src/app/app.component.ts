4e2c10baab228f4fdb2480f8f7c2c0a61ff452ba
Je-Mé Kruger-Baartjes <jmkb1502@gmail.com>
2025-01-08 10:23:26 +0200

    Removing redundant import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { ThemeToggleComponent } from './theme-toggle/theme-toggle.component';
 import { HeaderComponent } from './core/layout/header/header.component';
 
 
 @Component({
   selector: 'app-root', 
   standalone: true,
-  imports: [RouterOutlet, ThemeToggleComponent, HeaderComponent, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, HeaderComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
