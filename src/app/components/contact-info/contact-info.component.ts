948b6765874843258b1f65f3e8f096af5e95a6e7
Je-Mé Kruger-Baartjes <jmkb1502@gmail.com>
2025-02-25 20:08:28 +0200

    Add contact Info Section

--- src/app/components/contact-info/contact-info.component.ts
+++ src/app/components/contact-info/contact-info.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-info',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatIconModule, CommonModule],
   templateUrl: './contact-info.component.html',
   styleUrl: './contact-info.component.css'
 })
 export class ContactInfoComponent {
-
+  contactDetails = [
+    {
+      type: 'Email',
+      value: 'jmkb1502@gmail.com',
+      icon: 'email',
+      link: 'mailto:jmkb1502@gmail.com'
+    },
+    {
+      type: 'LinkedIn',
+      value: 'linkedin.com/in/je-mekruger-baartjes',
+      icon: 'linkedin',
+      link: 'https://www.linkedin.com/in/je-mekruger-baartjes'
+    },
+    {
+      type: 'GitHub',
+      value: 'github.com/Jem-Kaybi',
+      icon: 'code',
+      link: 'https://github.com/Jem-Kaybi'
+    },
+    {
+      type: 'Phone',
+      value: '+27 84 529 3333',
+      icon: 'phone',
+      link: 'tel:+27845293333'
+    }
+  ];
 }
