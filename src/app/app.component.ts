58a7d9a890e845388ff61a00ca9e7293a1648ed5
Je-Mé Kruger-Baartjes <jmkb1502@gmail.com>
2025-02-25 20:46:35 +0200

    Fix Dark mode on contact info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { OverlayContainer } from '@angular/cdk/overlay';
 import { HeaderComponent } from './core/layout/header/header.component';
 
 
@@ -12,4 +13,35 @@ import { HeaderComponent } from './core/layout/header/header.component';
 })
 export class AppComponent {
   title = 'personal-website';
+  isDarkMode = false;
+
+  constructor(private overlayContainer: OverlayContainer) {
+    if (typeof window !== 'undefined' && localStorage) {
+      const storedTheme = localStorage.getItem('darkMode');
+      this.isDarkMode = storedTheme === 'true';
+    }
+    this.applyTheme();
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    if (typeof window !== 'undefined' && localStorage) {
+      localStorage.setItem('darkMode', String(this.isDarkMode));
+    }
+    this.applyTheme();
+  }
+
+  private applyTheme() {
+    const darkClass = 'dark-theme';
+    
+    if (typeof document !== 'undefined') {
+      const root = document.documentElement; // Ensure document exists before using it
+  
+      if (this.isDarkMode) {
+        root.classList.add(darkClass);
+      } else {
+        root.classList.remove(darkClass);
+      }
+    }
+  }
 }
