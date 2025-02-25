037d948349ab1137d5cf93fa101f1795ca2499ec
Je-Mé Kruger-Baartjes <jmkb1502@gmail.com>
2025-02-25 20:08:48 +0200

    Add education Section

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card'; 
 
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './education.component.html',
   styleUrl: './education.component.css'
 })
 export class EducationComponent {
-
+  educationList = [
+    {
+      institution: 'University of the Witwatersrand',
+      degree: 'Bachelor of Science Honours - BSc Hons, Computer Science',
+      years: '2022',
+      logo: 'assets/wits-logo.png', // Add a logo if you want
+    },
+    {
+      institution: 'University of the Witwatersrand',
+      degree: 'Bachelor of Science - BS, Computer Science and Information Systems',
+      years: '2018 - 2021',
+      logo: 'assets/wits-logo.png',
+    },
+    {
+      institution: 'Northcliff High School',
+      degree: 'High School Diploma',
+      years: '2013 - 2017',
+      logo: 'assets/northcliff-logo.png',
+    }
+  ];
 }
+
